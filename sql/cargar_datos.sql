5fc048475d9569747b49b24df7e3b6836552db29
Javier Aparicio <javi.2062003@gmail.com>
2025-07-07 18:12:32 +0200

    Cargar datos en tablas

---
+++ sql/cargar_datos.sql
@@ -0,0 +1,8 @@
+COPY marketing FROM 'C:\temp-data\marketing.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY soporte FROM 'C:\temp-data\soporte.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY ventas FROM 'C:\temp-data\ventas.csv'
+DELIMITER ',' CSV HEADER;
